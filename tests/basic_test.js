7807659f88dab97db73dfa98ffda29f847fb9682
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2014-08-18 04:33:00 +0300

    test min, max, and sum aggregators

--- tests/basic_test.js
+++ tests/basic_test.js
@@ -34,13 +34,13 @@ describe('Check sensor fields: ', function() {
 
 		var newsensor = new ibisense.models.Sensor({
 			name           : "Mocha test sensor",
-            description    : "this is a test sensor that will be deleted after the test run!",
-            latitude       : 60.203381,
-            longitude      : 24.778236,
-            indoor_x_coord : 1.0,
-            indoor_y       : 1.1,
-            indoor_id      : 1,
-            attributes     : {"type": "test"}
+			description    : "this is a test sensor that will be deleted after the test run!",
+			latitude       : 60.203381,
+			longitude      : 24.778236,
+			indoor_x_coord : 1.0,
+			indoor_y       : 1.1,
+			indoor_id      : 1,
+			attributes     : {"type": "test"}
 		});
 
 		var createNewSensor = function(newsensor) {
@@ -346,6 +346,127 @@ describe('Add and get datapoitns:', function() {
 	});
 });
 
+describe('Add and get datapoitns:', function() {
+	it('should correctly create sensor, create channel, insert and read out datapoints with time zone offset and delete sensor', function(done) {
+		this.timeout(60000);
+
+		var newsensor = new ibisense.models.Sensor({name: "Mocha test sensor"});
+		var newchannel = new ibisense.models.Channel({name: "Mocha test channel"});
+
+		var prepareData = function(start, end, step) {
+			return _.map(_.range(start, end, step), function(ts, i) {
+				return new ibisense.models.DataPoint({date: ts, value: i});
+			});
+		}
+
+		var createNewSensor = function(newsensor) {
+			var dfd = when.defer();
+			ibisense.sensors.add(newsensor, function(sensor) {
+				dfd.resolve(sensor);
+			}, function(error) {
+				dfd.reject(error);
+			});
+			return dfd.promise;
+		}
+
+		var createNewChannel = function(suid, newchannel) {
+			var dfd = when.defer();
+			ibisense.channels.add(suid, newchannel, function(channel) {
+				dfd.resolve(channel);
+			}, function(error) {
+				dfd.reject(error);
+			});
+			return dfd.promise;
+		}
+
+		var addDatapoints = function(cuid, datapoints) {
+			var dfd = when.defer();
+
+			ibisense.datapoints.add(cuid, datapoints, function() {
+				dfd.resolve();
+			}, function(error) {
+				dfd.reject(error);
+			});
+
+			return dfd.promise;
+		}
+
+		var queryDatapoints = function(cuid, start, end, tz) {
+			var dfd = when.defer();
+			var query = {
+				cuid  : cuid,
+				start : new Date(start).toISOString(),
+				end   : new Date(end).toISOString(),
+				tz    : tz
+			};
+
+			ibisense.datapoints.get(query, function(dataset) {
+				dfd.resolve(dataset.datapoints());
+			}, function(error) {
+				dfd.reject(dataset.datapoints);
+			});
+
+			return dfd.promise;
+		}
+
+		var deleteSensor = function(suid) {
+			ibisense.sensors.remove(suid);
+		}
+
+		var cuid  = undefined;
+
+		var num   = 100;  
+		var step  = 1000; // 1 second
+
+		var end    = new Date().getTime();
+		var start  = end - num * step;
+		var tz     = "Europe/Helsinki";
+		var offset = 3*60*60*1000; 
+		var original_datapoints = prepareData(start, end + 1, step);
+
+		var propagation_delay = 5000; // wait 5 seconds before attempting to read out data
+		
+		when(createNewSensor(newsensor)).then(function(sensor) {
+			newsensor = sensor;
+			when(createNewChannel(newsensor.suid(), newchannel)).then(function(created_channel) {
+				newchannel = created_channel;
+				cuid = newchannel.cuid();
+				when(addDatapoints(cuid, original_datapoints)).then(function(received_datapoints) {
+					setTimeout(function() {
+						when(queryDatapoints(cuid, start, end, tz)).then(
+						function(received_datapoints) {
+							assert.equal(received_datapoints.length, original_datapoints.length);
+							_.each(received_datapoints, function(dp, i) {
+								var odp = original_datapoints[i];
+								try {
+									assert.equal(odp.timestampMs() + offset, dp.timestampMs());
+									assert.equal(odp.value(), dp.value());
+								} catch(error) {
+									deleteSensor(fetched_sensor.suid());
+									done(error);
+								}
+							});
+							deleteSensor(newsensor.suid());
+							done();
+						}, function(error) {
+							deleteSensor(newsensor.suid());
+							done(error);
+						});
+					}, propagation_delay);
+				}, function(error) {
+					deleteSensor(newsensor.suid());
+					done(error);
+				});
+			}, function(error) {
+				deleteSensor(newsensor.suid());
+				done(error);
+			});
+		}, function(error) { 
+			done(error);
+		});
+	});
+});
+
 
 describe('Add and get with rollups:', function() {
 	it('should correctly create sensor, create channel, insert and rollup datapoints into 10 seconds bins using average aggregator function and delete sensor', function(done) {
@@ -506,6 +627,482 @@ describe('Add and get with rollups:', function() {
 	});
 });
 
+describe('Add and get with rollups:', function() {
+	it('should correctly create sensor, create channel, insert and rollup datapoints into 10 seconds bins using sum aggregator function and delete sensor', function(done) {
+		this.timeout(120000);
+
+		var newsensor = new ibisense.models.Sensor({name: "Mocha test sensor"});
+		var newchannel = new ibisense.models.Channel({name: "Mocha test channel"});
+
+		var prepareData = function(start, end, step) {
+			return _.map(_.range(start, end, step), function(ts, i) {
+				return new ibisense.models.DataPoint({date: ts, value: i});
+			});
+		}
+
+		var createNewSensor = function(newsensor) {
+			var dfd = when.defer();
+			ibisense.sensors.add(newsensor, function(sensor) {
+				dfd.resolve(sensor);
+			}, function(error) {
+				dfd.reject(error);
+			});
+			return dfd.promise;
+		}
+
+		var createNewChannel = function(suid, newchannel) {
+			var dfd = when.defer();
+			ibisense.channels.add(suid, newchannel, function(channel) {
+				dfd.resolve(channel);
+			}, function(error) {
+				dfd.reject(error);
+			});
+			return dfd.promise;
+		}
+
+		var addDatapoints = function(cuid, datapoints) {
+			var dfd = when.defer();
+
+			ibisense.datapoints.add(cuid, datapoints, function() {
+				dfd.resolve();
+			}, function(error) {
+				dfd.reject(error);
+			});
+
+			return dfd.promise;
+		}
+
+		var queryDatapoints = function(cuid, start, end, func, interval) {
+			var dfd = when.defer();
+			var query = {
+				cuid: cuid,
+				start: new Date(start).toISOString(),
+				end: new Date(end).toISOString(),
+				func: func, 
+				interval: interval
+			};
+
+			ibisense.datapoints.get(query, function(dataset) {
+				dfd.resolve(dataset.datapoints());
+			}, function(error) {
+				dfd.reject(dataset.datapoints);
+			});
+
+			return dfd.promise;
+		}
+
+		var aggregate = function(datapoints) {
+			var aggregated_datapoints = [];
+
+			var accumulator = 0;
+			var pivot = null;
+			var interval = 10;
+
+			_.each(datapoints, function(dp, i) {
+
+				if (( (i + 1) % interval) == 1 ) {
+					pivot = dp.timestamp();
+				}
+
+				if (( (i + 1) % interval) == 0) {
+					accumulator += dp.value();
+					aggregated_datapoints.push(
+						new ibisense.models.DataPoint({
+							date: pivot, 
+							value: accumulator
+						})
+					);
+					accumulator = 0;
+				} else {
+					accumulator += dp.value();
+				}
+
+				if (datapoints.length - 1 == i) {
+					aggregated_datapoints.push(
+						new ibisense.models.DataPoint({
+							date: pivot, 
+							value: accumulator
+						})
+					);
+				}
+			});
+			return aggregated_datapoints;
+		}
+
+		var deleteSensor = function(suid) {
+			ibisense.sensors.remove(suid);
+		}
+
+		var cuid  = undefined;
+
+		var num   = 100;  
+		var step  = 1000; // 1 second
+
+		var end   = new Date().getTime();
+		var start = end - num * step;
+		var original_datapoints = prepareData(start, end + 1, step);
+		var aggregated_datapoints = aggregate(original_datapoints);
+		var propagation_delay   = 5000; // wait 5 seconds before attempting to read out data
+		var func  = "sum";
+		var ival  = "10s"; 
+
+		when(createNewSensor(newsensor)).then(function(sensor) {
+			newsensor = sensor;
+			when(createNewChannel(newsensor.suid(), newchannel))
+				.then(function(created_channel) {
+				newchannel = created_channel;
+				cuid = newchannel.cuid();
+				when(addDatapoints(cuid, original_datapoints))
+					.then(function(received_datapoints) {
+					setTimeout(function() {
+						when(queryDatapoints(cuid, start, end, func, ival))
+						.then(function(received_datapoints) {
+							assert.equal(received_datapoints.length, Math.ceil(original_datapoints.length / 10));
+							deleteSensor(newsensor.suid());
+
+							_.each(received_datapoints, function(dp, i) {
+								var dpa = aggregated_datapoints[i];
+								assert.equal(dp.timestampMs(), dpa.timestampMs());
+								assert.equal(dp.value(), dpa.value());
+							});
+
+							done();
+						}, function(error) {
+							deleteSensor(newsensor.suid());
+							done(error);
+						});
+					}, propagation_delay);
+				}, function(error) {
+					deleteSensor(newsensor.suid());
+					done(error);
+				});
+			}, function(error) {
+				deleteSensor(newsensor.suid());
+				done(error);
+			});
+		}, function(error) { 
+			done(error);
+		});
+	});
+});
+
+describe('Add and get with rollups:', function() {
+	it('should correctly create sensor, create channel, insert and rollup datapoints into 10 seconds bins using max aggregator function and delete sensor', function(done) {
+		this.timeout(120000);
+
+		var newsensor = new ibisense.models.Sensor({name: "Mocha test sensor"});
+		var newchannel = new ibisense.models.Channel({name: "Mocha test channel"});
+
+		var prepareData = function(start, end, step) {
+			return _.map(_.range(start, end, step), function(ts, i) {
+				return new ibisense.models.DataPoint({date: ts, value: i});
+			});
+		}
+
+		var createNewSensor = function(newsensor) {
+			var dfd = when.defer();
+			ibisense.sensors.add(newsensor, function(sensor) {
+				dfd.resolve(sensor);
+			}, function(error) {
+				dfd.reject(error);
+			});
+			return dfd.promise;
+		}
+
+		var createNewChannel = function(suid, newchannel) {
+			var dfd = when.defer();
+			ibisense.channels.add(suid, newchannel, function(channel) {
+				dfd.resolve(channel);
+			}, function(error) {
+				dfd.reject(error);
+			});
+			return dfd.promise;
+		}
+
+		var addDatapoints = function(cuid, datapoints) {
+			var dfd = when.defer();
+
+			ibisense.datapoints.add(cuid, datapoints, function() {
+				dfd.resolve();
+			}, function(error) {
+				dfd.reject(error);
+			});
+
+			return dfd.promise;
+		}
+
+		var queryDatapoints = function(cuid, start, end, func, interval) {
+			var dfd = when.defer();
+			var query = {
+				cuid: cuid,
+				start: new Date(start).toISOString(),
+				end: new Date(end).toISOString(),
+				func: func, 
+				interval: interval
+			};
+
+			ibisense.datapoints.get(query, function(dataset) {
+				dfd.resolve(dataset.datapoints());
+			}, function(error) {
+				dfd.reject(dataset.datapoints);
+			});
+
+			return dfd.promise;
+		}
+
+		var aggregate = function(datapoints) {
+			var aggregated_datapoints = [];
+
+			var accumulator = Number.MIN_VALUE;
+			var pivot = null;
+			var interval = 10;
+
+			_.each(datapoints, function(dp, i) {
+
+				if (( (i + 1) % interval) == 1 ) {
+					pivot = dp.timestamp();
+				}
+
+				if (( (i + 1) % interval) == 0) {
+					if (accumulator < dp.value()) accumulator = dp.value();
+					aggregated_datapoints.push(
+						new ibisense.models.DataPoint({
+							date: pivot, 
+							value: accumulator
+						})
+					);
+					accumulator = Number.MIN_VALUE;
+				} else {
+					if (accumulator < dp.value()) accumulator = dp.value();
+				}
+
+				if (datapoints.length - 1 == i) {
+					aggregated_datapoints.push(
+						new ibisense.models.DataPoint({
+							date: pivot, 
+							value: accumulator
+						})
+					);
+				}
+			});
+			return aggregated_datapoints;
+		}
+
+		var deleteSensor = function(suid) {
+			ibisense.sensors.remove(suid);
+		}
+
+		var cuid  = undefined;
+
+		var num   = 100;  
+		var step  = 1000; // 1 second
+
+		var end   = new Date().getTime();
+		var start = end - num * step;
+		var original_datapoints = prepareData(start, end + 1, step);
+		var aggregated_datapoints = aggregate(original_datapoints);
+		var propagation_delay   = 5000; // wait 5 seconds before attempting to read out data
+		var func  = "max";
+		var ival  = "10s"; 
+
+		when(createNewSensor(newsensor)).then(function(sensor) {
+			newsensor = sensor;
+			when(createNewChannel(newsensor.suid(), newchannel))
+				.then(function(created_channel) {
+				newchannel = created_channel;
+				cuid = newchannel.cuid();
+				when(addDatapoints(cuid, original_datapoints))
+					.then(function(received_datapoints) {
+					setTimeout(function() {
+						when(queryDatapoints(cuid, start, end, func, ival))
+						.then(function(received_datapoints) {
+							assert.equal(received_datapoints.length, Math.ceil(original_datapoints.length / 10));
+							deleteSensor(newsensor.suid());
+
+							_.each(received_datapoints, function(dp, i) {
+								var dpa = aggregated_datapoints[i];
+								assert.equal(dp.timestampMs(), dpa.timestampMs());
+								assert.equal(dp.value(), dpa.value());
+							});
+
+							done();
+						}, function(error) {
+							deleteSensor(newsensor.suid());
+							done(error);
+						});
+					}, propagation_delay);
+				}, function(error) {
+					deleteSensor(newsensor.suid());
+					done(error);
+				});
+			}, function(error) {
+				deleteSensor(newsensor.suid());
+				done(error);
+			});
+		}, function(error) { 
+			done(error);
+		});
+	});
+});
+
+describe('Add and get with rollups:', function() {
+	it('should correctly create sensor, create channel, insert and rollup datapoints into 10 seconds bins using min aggregator function and delete sensor', function(done) {
+		this.timeout(120000);
+
+		var newsensor = new ibisense.models.Sensor({name: "Mocha test sensor"});
+		var newchannel = new ibisense.models.Channel({name: "Mocha test channel"});
+
+		var prepareData = function(start, end, step) {
+			return _.map(_.range(start, end, step), function(ts, i) {
+				return new ibisense.models.DataPoint({date: ts, value: i});
+			});
+		}
+
+		var createNewSensor = function(newsensor) {
+			var dfd = when.defer();
+			ibisense.sensors.add(newsensor, function(sensor) {
+				dfd.resolve(sensor);
+			}, function(error) {
+				dfd.reject(error);
+			});
+			return dfd.promise;
+		}
+
+		var createNewChannel = function(suid, newchannel) {
+			var dfd = when.defer();
+			ibisense.channels.add(suid, newchannel, function(channel) {
+				dfd.resolve(channel);
+			}, function(error) {
+				dfd.reject(error);
+			});
+			return dfd.promise;
+		}
+
+		var addDatapoints = function(cuid, datapoints) {
+			var dfd = when.defer();
+
+			ibisense.datapoints.add(cuid, datapoints, function() {
+				dfd.resolve();
+			}, function(error) {
+				dfd.reject(error);
+			});
+
+			return dfd.promise;
+		}
+
+		var queryDatapoints = function(cuid, start, end, func, interval) {
+			var dfd = when.defer();
+			var query = {
+				cuid: cuid,
+				start: new Date(start).toISOString(),
+				end: new Date(end).toISOString(),
+				func: func, 
+				interval: interval
+			};
+
+			ibisense.datapoints.get(query, function(dataset) {
+				dfd.resolve(dataset.datapoints());
+			}, function(error) {
+				dfd.reject(dataset.datapoints);
+			});
+
+			return dfd.promise;
+		}
+
+		var aggregate = function(datapoints) {
+			var aggregated_datapoints = [];
+
+			var accumulator = Number.MAX_VALUE;
+			var pivot = null;
+			var interval = 10;
+
+			_.each(datapoints, function(dp, i) {
+
+				if (( (i + 1) % interval) == 1 ) {
+					pivot = dp.timestamp();
+				}
+
+				if (( (i + 1) % interval) == 0) {
+					if (accumulator >= dp.value()) accumulator = dp.value();
+					aggregated_datapoints.push(
+						new ibisense.models.DataPoint({
+							date: pivot, 
+							value: accumulator
+						})
+					);
+					accumulator = Number.MAX_VALUE;
+				} else {
+					if (accumulator >= dp.value()) accumulator = dp.value();
+				}
+
+				if (datapoints.length - 1 == i) {
+					aggregated_datapoints.push(
+						new ibisense.models.DataPoint({
+							date: pivot, 
+							value: dp.value()
+						})
+					);
+				}
+			});
+			return aggregated_datapoints;
+		}
+
+		var deleteSensor = function(suid) {
+			ibisense.sensors.remove(suid);
+		}
+
+		var cuid  = undefined;
+
+		var num   = 100;  
+		var step  = 1000; // 1 second
+
+		var end   = new Date().getTime();
+		var start = end - num * step;
+		var original_datapoints = prepareData(start, end + 1, step);
+		var aggregated_datapoints = aggregate(original_datapoints);
+		var propagation_delay   = 5000; // wait 5 seconds before attempting to read out data
+		var func  = "min";
+		var ival  = "10s"; 
+
+		when(createNewSensor(newsensor)).then(function(sensor) {
+			newsensor = sensor;
+			when(createNewChannel(newsensor.suid(), newchannel))
+				.then(function(created_channel) {
+				newchannel = created_channel;
+				cuid = newchannel.cuid();
+				when(addDatapoints(cuid, original_datapoints))
+					.then(function(received_datapoints) {
+					setTimeout(function() {
+						when(queryDatapoints(cuid, start, end, func, ival))
+						.then(function(received_datapoints) {
+							assert.equal(received_datapoints.length, Math.ceil(original_datapoints.length / 10));
+							deleteSensor(newsensor.suid());
+
+							_.each(received_datapoints, function(dp, i) {
+								var dpa = aggregated_datapoints[i];
+								assert.equal(dp.timestampMs(), dpa.timestampMs());
+								assert.equal(dp.value(), dpa.value());
+							});
+
+							done();
+						}, function(error) {
+							deleteSensor(newsensor.suid());
+							done(error);
+						});
+					}, propagation_delay);
+				}, function(error) {
+					deleteSensor(newsensor.suid());
+					done(error);
+				});
+			}, function(error) {
+				deleteSensor(newsensor.suid());
+				done(error);
+			});
+		}, function(error) { 
+			done(error);
+		});
+	});
+});
 
 describe('Add and get with rollups:', function() {
 	it('should correctly create sensor, create channel, insert and rollup datapoints into 10 minutes bins using average aggregator function and delete sensor', function(done) {
